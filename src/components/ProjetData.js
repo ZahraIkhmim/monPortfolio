782e387020d5b6f54c0d5d21de513eba22400a57
ZahraIkhmim <zahraikhmim@gmail.com>
2024-10-08 14:23:54 +0200

    ajout du svg mode statique

--- src/components/ProjetData.js
+++ src/components/ProjetData.js
@@ -1,24 +1,13 @@
 import React from 'react';
+import mySvg from '../PalladioGraphV2.svg';
 
 const ProjetData = () => {
-  return (
-    <div>
-        <iframe 
-          title="House prices in the U.K." 
-          aria-label="Lignes multiples" 
-          id="datawrapper-chart-RFbPo" 
-          src="https://datawrapper.dwcdn.net/RFbPo/1/" 
-          scrolling="no" 
-          frameBorder="0" 
-          style={{ width: '100%', border: 'none' }} 
-          height="923" 
-          data-external="1"
-        ></iframe>
-        <script type="text/javascript">
-          {`!function(){"use strict".window.addEventListener("message",function(a){if(void 0!==a.data["datawrapper-height"]){var e=document.querySelectorAll("iframe");for(var t in a.data["datawrapper-height"])for(var r=0;r<e.length;r++)if(e[r].contentWindow===a.source){var i=a.data["datawrapper-height"][t]+"px";e[r].style.height=i}}})}();`}
-        </script>
-    </div>
-  );
-}
+    return (
+      <div style={{ width: '100%', height: 'auto' }}>
+        <h4>Les jeux vidéos et leur présence sur les différentes plateformes.</h4>
+        <img src={mySvg} alt="Mon SVG"/>
+      </div>
+    );
+  }  
 
 export default ProjetData;
